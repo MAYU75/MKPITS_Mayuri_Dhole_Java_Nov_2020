bc0a14e19f8a8e5db490348d60d81453320eb31c
Mayu <mayuridhole@gmail.com>
2021-04-05 15:01:38 +0530

    awtexamples Package programs added

--- Core_Java_Practice/src/com/mkpits/java/awtexamples/ExToDemonstrateHowToAddTextAreaInFrame.java
+++ Core_Java_Practice/src/com/mkpits/java/awtexamples/ExToDemonstrateHowToAddTextAreaInFrame.java
@@ -1,4 +1,22 @@
+//Java program to demonstrate how to add TextArea in Frame.
+
 package src.com.mkpits.java.awtexamples;
 
+import java.awt.*;
+
 public class ExToDemonstrateHowToAddTextAreaInFrame {
+    ExToDemonstrateHowToAddTextAreaInFrame() {
+        Frame f = new Frame();
+        TextArea area = new TextArea("Hello. Welcome to Java programming.");
+        area.setBounds(10, 30, 300, 300);
+        f.add(area);
+        f.setSize(400, 400);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ExToDemonstrateHowToAddTextAreaInFrame();
+    }
+
 }
