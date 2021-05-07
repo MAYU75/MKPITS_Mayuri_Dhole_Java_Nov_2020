2f787a840021d5e4f240caafa430cc2550a84087
Mayu <mayuridhole@gmail.com>
2021-05-07 17:39:06 +0530

    Programs added in package named array

---
+++ Core_Java_Practice/src/com/mkpits/java/array/ExToDetermineWhetherGivenMatrixIsASparseMatrix.java
@@ -0,0 +1,36 @@
+//Java program to determine whether a given matrix is a sparse matrix.
+
+package src.com.mkpits.java.array;
+
+public class ExToDetermineWhetherGivenMatrixIsASparseMatrix {
+    public static void main(String[] args) {
+        int rows, cols, size, count = 0;
+
+        //Initialize matrix a
+        int a[][] = {
+                {4, 0, 0},
+                {0, 5, 0},
+                {0, 0, 6}
+        };
+
+        //Calculates number of rows and columns present in given matrix
+        rows = a.length;
+        cols = a[0].length;
+
+        //Calculates the size of array
+        size = rows * cols;
+
+        //Count all zero element present in matrix
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (a[i][j] == 0)
+                    count++;
+            }
+        }
+
+        if (count > (size / 2))
+            System.out.println("Given matrix is a sparse matrix");
+        else
+            System.out.println("Given matrix is not a sparse matrix");
+    }
+}
