03d52322cf84348b05d8fb188ba70e52811514e2
Mayu <mayuridhole@gmail.com>
2021-06-02 22:35:52 +0530

    README.md file updated and Programs added to package arraydeque

---
+++ Core_Java_Practice/src/com/mkpits/java/arraydeque/ArrayDequeExToAddAndTraverseBooksInDeque.java
@@ -0,0 +1,6 @@
+//Java program to add and traverse books in
+
+package src.com.mkpits.java.arraydeque;
+
+public class ArrayDequeExToAddAndTraverseBooksInDequeue {
+}
---
+++ Core_Java_Practice/src/com/mkpits/java/arraydeque/ArrayDequeExToAddAndTraverseElements.java
@@ -0,0 +1,2 @@
+package src.com.mkpits.java.arraydeque;public class ArrayDequeExToAddAndTraverseElements {
+}
